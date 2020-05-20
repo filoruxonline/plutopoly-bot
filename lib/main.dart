559add80a7b5713d3b1eb186606438fd9ec1d639
filoruxonline <63004237+filoruxonline@users.noreply.github.com>
2020-05-20 21:49:56 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:PlutopolyExtension/helper/init_bot.dart';
 //Enter a game pin here (replace null)
 const String GAMEPIN = null;
 //Enter a unique project name as author:botName
-const String PROJECTNAME = "filorux.ex";
+const String PROJECTNAME = "filorux.example";
 
 //If false it will not listen to any updates
 //the bot will finish after the onInit() call
@@ -16,7 +16,7 @@ const Map<String, dynamic> meta = {
   "projectname": PROJECTNAME,
   //Description, use \n for newline.
   "description":
-      "A plutopoly bot\nThis is a very cool bot that brings the game to a totally new level.",
+      "A plutopoly bot.",
   "listen": GAMELISTENER,
 
   //overide the author name with your full name
